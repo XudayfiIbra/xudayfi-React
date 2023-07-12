41ea0267484977c0bfba5a46f67374cbe5ffd6cf
xudayfi <xudayfiibraahim8@gmail.com>
2023-07-12 07:17:39 +0300

    added form

--- xudayfi/src/index.js
+++ xudayfi/src/index.js
@@ -1,6 +1,10 @@
 import { createRoot } from "react-dom/client";
 
-let greeting = <h2>hello, welcome to react js</h2>;
+let greeting = 
+  <div>
+    <label>Enter your full name</label>
+    <input type="text" placeholder="John alex fen"></input>
+  </div>
 
 const container = document.getElementById("root");
 const root = createRoot(container);
